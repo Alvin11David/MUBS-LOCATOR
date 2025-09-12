5428e4171af84b6131a2753c6a70242491d1b21b
Alvin11David <alvin69david@gmail.com>
2025-09-12 06:51:47 +0300

    Added the text

--- lib/user pages/splash/splash_screen.dart
+++ lib/user pages/splash/splash_screen.dart
@@ -48,15 +48,60 @@ class SplashScreen extends StatelessWidget {
                             angle: -rotationAngle,
                             child: Stack(
                               children: [
+                                // Image
                                 Align(
-                                  alignment: Alignment.centerLeft, // 👈 Move image to the left
+                                  alignment: Alignment.centerLeft,
                                   child: Padding(
-                                    padding: EdgeInsets.only(right: screenWidth * 0.8), // fine-tune spacing
+                                    padding: EdgeInsets.only(
+                                        right: screenWidth * 0.8),
                                     child: Image.asset(
                                       'assets/images/ambasizejackline.png',
                                       width: screenWidth * 1.9,
                                       height: screenHeight * 1.9,
                                       fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                ),
+
+                                // Positioned Text on top of image
+                                Positioned(
+                                  left: screenWidth * 0.1,
+                                  top: screenHeight * 0.2,
+                                  child: Text(
+                                    'AMBASIZE',
+                                    style: TextStyle(
+                                      fontSize: screenWidth * 0.09,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontFamily: 'Poppins',
+                                      shadows: [
+                                        Shadow(
+                                          offset: Offset(0, 0),
+                                          blurRadius: 3.0,
+                                          color: Colors.black,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                // Positioned Text on top of image
+                                Positioned(
+                                  left: screenWidth * 0.4,
+                                  top: screenHeight * 0.25,
+                                  child: Text(
+                                    'JACKLINE',
+                                    style: TextStyle(
+                                      fontSize: screenWidth * 0.09,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontFamily: 'Poppins',
+                                      shadows: [
+                                        Shadow(
+                                          offset: Offset(0, 0),
+                                          blurRadius: 3.0,
+                                          color: Colors.black,
+                                        ),
+                                      ],
                                     ),
                                   ),
                                 ),
