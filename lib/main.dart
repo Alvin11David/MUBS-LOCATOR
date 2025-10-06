f86ccd87d3823aa982b4d5f701873670bffd6142
sekibaalamark <markmajje256@gmail.com>
2025-10-06 16:24:45 +0300

    set up main

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:mubs_locator/admin%20pages/admin_dashboard.dart';
+import 'package:mubs_locator/admin%20pages/dashboards/add_place_screen.dart';
+import 'package:mubs_locator/admin%20pages/dashboards/location_management.dart';
 import 'package:mubs_locator/firebase_options.dart';
+import 'package:mubs_locator/user%20pages/auth/forgot_password.dart';
+import 'package:mubs_locator/user%20pages/auth/otp_screen.dart';
+import 'package:mubs_locator/user%20pages/auth/set_password_screen.dart';
+import 'package:mubs_locator/user%20pages/auth/sign_in.dart';
+import 'package:mubs_locator/user%20pages/auth/sign_up.dart';
 import 'package:mubs_locator/user%20pages/intro/onboarding_screen1.dart';
 import 'package:mubs_locator/user%20pages/intro/onboarding_screen2.dart';
 import 'package:mubs_locator/user%20pages/intro/onboarding_screen3.dart';
-import 'package:mubs_locator/user%20pages/splash/splash_screen.dart';
+import 'package:mubs_locator/user%20pages/map%20screens/home_screen.dart';
+import 'package:mubs_locator/user%20pages/map%20screens/location_select_screen.dart';
 import 'package:mubs_locator/user%20pages/other%20screens/about_screen.dart';
-import 'package:mubs_locator/user%20pages/other%20screens/terms_and_privacy_screen.dart';
+import 'package:mubs_locator/user%20pages/other%20screens/edit_profile_screen.dart';
+import 'package:mubs_locator/user%20pages/other%20screens/notification_screen.dart';
+import 'package:mubs_locator/user%20pages/splash/splash_screen.dart';
+import 'package:mubs_locator/user%20pages/other%20screens/profile_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    print('Firebase initialized successfully');
+  } catch (e) {
+    print('Firebase init error: $e');
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,14 +44,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      initialRoute: '/SplashScreen', 
       routes: {
-       //'/': (context) => const SplashScreen(),
-       //'/OnboardingScreen1': (context) => const OnboardingScreen1(),
-       //'/OnboardingScreen2': (context) => const OnboardingScreen2(),
-       //'/OnboardingScreen3': (context) => const OnboardingScreen3(),
-       
-       //'/': (context) => const AboutScreen(),
-        '/': (context) => const TermsAndPrivacyScreen(),
+        '/OnboardingScreen1': (context) => const OnboardingScreen1(),
+        '/OnboardingScreen2': (context) => const OnboardingScreen2(),
+        '/OnboardingScreen3': (context) => const OnboardingScreen3(),
+        '/SignInScreen': (context) => const SignInScreen(),
+        '/SplashScreen': (context) => const SplashScreen(),
+        '/SignUpScreen': (context) => const SignUpScreen(),
+        '/ForgotPasswordScreen': (context) => const ForgotPasswordScreen(),
+        '/LocationSelectScreen': (context) => const LocationSelectScreen(),
+        '/AboutScreen': (context) => const AboutScreen(),
+        '/EditProfileScreen': (context) => const EditProfileScreen(),
+        '/AdminDashboardScreen': (context) => const AdminDashboardScreen(),
+        '/LocationManagementScreen': (context) => const LocationManagementScreen(),
+        '/ProfileScreen': (context) => const ProfileScreen(),
+        '/OTPScreen': (context) => const OTP_Screen(email: ''),
+        '/ResetPasswordScreen': (context) => const ResetPasswordScreen(email: '',),
+        '/NotificationsScreen': (context) => const NotificationsScreen(),
+        '/AddPlaceScreen': (context) => const AddPlaceScreen(),
+        '/HomeScreen': (context) => const HomeScreen(),
       },
     );
   }
@@ -82,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
